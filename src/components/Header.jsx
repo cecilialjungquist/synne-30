1ce4f5bf11430acd13c62db11c928539138ea4ff
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-10-06 11:31:33 +0200

    Styling update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 function Header() {
-    return (  
+    return (
         <header>
             <h1>DIRTY THIRTY!</h1>
-            <section className="box-container">
-                <div className="pink-box"></div>
-                <div className="gif-box">
-                    <img src="https://media.giphy.com/media/xTiQyKDyF0rjHXSOCA/giphy.gif"/>
-                </div>
-            </section>
+            <div className="pink-box"></div>
+            <div className="gif-box">
+                <img src="https://media.giphy.com/media/xTiQyKDyF0rjHXSOCA/giphy.gif" />
+            </div>
         </header>
     );
 }
